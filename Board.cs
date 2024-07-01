8adb58cd448f2296226f20d0d781ae6eb7d14cac
OneVth <onebrotravel@gmail.com>
2024-07-01 21:17:46 +0900

    Implement Board class

---
+++ Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public static  class Board
+    {
+        public static char[] gameBoard;
+
+        static Board()
+        {
+            gameBoard = new char[9];
+
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                gameBoard[i] = ' ';
+            }
+        }
+    }
+}
--- obj/Debug/netcoreapp3.1/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd65222a6c870dbc68691d39600484a73496372")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
