f4df12c3e9110139d6c7ee6af4bb5d952f0654ac
OneVth <onebrotravel@gmail.com>
2024-07-05 12:29:49 +0900

    Implement GameManager

--- GameManager.cs
+++ GameManager.cs
@@ -7,52 +7,198 @@ namespace TicTacToe
     public static class GameManager
     {
         public static Player[] Players;
+        public static Player Winner;
+        public static readonly string IntroText = "Start TicTacToe game!\n";
+        public static string EndingText =>
+            @$"Game Over! Winner is {Winner.Name}.
+Press Q to quit or other to restart.";
 
-        public static readonly string IntroText = "Start TicTacToe game!";
-        public static readonly string EndingText;
+        static bool isGameover = false;
 
         public static void StartGame()
         {
+            Console.Clear();
             Console.WriteLine(IntroText);
 
-            while (true)
+            Players = new Player[2];
+
+            for (int i = 0; i < Players.Length; i++)
+            {
+                Console.Write($"Enter Player{i + 1}'s name: ");
+                string name = Console.ReadLine();
+                Console.Write($"Enter Player{i + 1}'s mark: ");
+                char mark = Console.ReadKey().KeyChar;
+                Players[i] = new Player(name, mark);
+                Console.WriteLine($"\nPlayers{i + 1}'s name: {Players[i].Name}, mark: {Players[i].Mark}");
+            }
+
+            Console.WriteLine("Let's start the game!");
+            Console.ReadLine();
+
+            while (!isGameover)
             {
                 Console.Clear();
                 DisplayGameLayout(Board.GameBoard);
 
-                ChoosePosition();
-                CheckWinner();
-
+                for (int i = 0; i < Players.Length; i++)
+                {
+                    // isGameover가 true여도 i가 모두 돌기 전까지 게임이 실행되는 이슈
+                    ChoosePosition(Players[i]);
+                    Console.Clear();
+                    DisplayGameLayout(Board.GameBoard);
+                    CheckWinner(Board.GameBoard, Players[i], out Winner, out isGameover);
+                    if (isGameover) break;
+                }
             }
+
+            Console.WriteLine(EndingText);
+            if (Console.ReadKey().KeyChar == 'q') return;
+            else RestartGame(Board.GameBoard);
         }
 
         /// <summary>
         /// Draw Layout on console window
         /// </summary>
-        public static void DisplayGameLayout(char[,] _gameBoard)
+        public static void DisplayGameLayout(char[,] gameBoard)
         {
             Console.WriteLine(" -----------");
-            Console.WriteLine($"| {_gameBoard[0, 0]} | {_gameBoard[0, 1]} | {_gameBoard[0, 2]} |");
+            Console.WriteLine($"| {gameBoard[0, 0]} | {gameBoard[0, 1]} | {gameBoard[0, 2]} |");
             Console.WriteLine(" -----------");
-            Console.WriteLine($"| {_gameBoard[1, 0]} | {_gameBoard[1, 1]} | {_gameBoard[1, 2]} |");
+            Console.WriteLine($"| {gameBoard[1, 0]} | {gameBoard[1, 1]} | {gameBoard[1, 2]} |");
             Console.WriteLine(" -----------");
-            Console.WriteLine($"| {_gameBoard[2, 0]} | {_gameBoard[2, 1]} | {_gameBoard[2, 2]} |");
+            Console.WriteLine($"| {gameBoard[2, 0]} | {gameBoard[2, 1]} | {gameBoard[2, 2]} |");
             Console.WriteLine(" -----------");
         }
 
-        public static void ChoosePosition()
+        public static void ChoosePosition(Player player)
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                Console.WriteLine($"{player.Name}'s turn! Choose row number(1 ~ 3)");
+                if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int row))
+                {
+                    if (row > 0 && row <= 3)
+                    {
+                        Console.WriteLine("\nChoose column number(1 ~ 3)");
+                        if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int col))
+                        {
+                            if (col > 0 && col <= 3)
+                            {
+                                if (Board.GameBoard[row - 1, col - 1] == ' ')
+                                {
+                                    Board.GameBoard[row - 1, col - 1] = player.Mark;
+                                    break;
+                                }
+                                else
+                                    Console.WriteLine("\nIt's already filled");
+                            }
+                            else
+                                Console.WriteLine("\nEnter a valid number");
+                        }
+                        else
+                            Console.WriteLine("\nYou could enter a number");
+                    }
+                    else
+                        Console.WriteLine("\nEnter a valid number");
+                }
+                else
+                    Console.WriteLine("\nYou could enter a number");
+            }
+
         }
 
-        public static bool CheckWinner()
+        public static void CheckWinner(char[,] gameBoard, Player player, out Player winner, out bool isGameover)
         {
-            throw new NotImplementedException();
+            if (gameBoard[0, 0] == player.Mark &&
+                gameBoard[0, 1] == player.Mark &&
+                gameBoard[0, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 0] == player.Mark &&
+                gameBoard[1, 1] == player.Mark &&
+                gameBoard[2, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 0] == player.Mark &&
+                    gameBoard[1, 0] == player.Mark &&
+                    gameBoard[2, 0] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 1] == player.Mark &&
+                    gameBoard[1, 1] == player.Mark &&
+                    gameBoard[2, 1] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 2] == player.Mark &&
+                    gameBoard[1, 2] == player.Mark &&
+                    gameBoard[2, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[1, 0] == player.Mark &&
+                    gameBoard[1, 1] == player.Mark &&
+                    gameBoard[1, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 2] == player.Mark &&
+                    gameBoard[1, 1] == player.Mark &&
+                    gameBoard[2, 0] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[2, 0] == player.Mark &&
+                    gameBoard[2, 1] == player.Mark &&
+                    gameBoard[2, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            if (gameBoard[0, 0] == player.Mark &&
+                    gameBoard[0, 1] == player.Mark &&
+                    gameBoard[0, 2] == player.Mark)
+            {
+                isGameover = true;
+                winner = player;
+                return;
+            }
+
+            isGameover = false;
+            winner = null;
         }
 
-        public static void ResetGame() 
+        public static void RestartGame(char[,] gameBoard)
         {
-            throw new NotImplementedException();
+            isGameover = false;
+            Board.Initialize(gameBoard);
+            StartGame();
         }
     }
 }
